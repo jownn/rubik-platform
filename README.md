15345ea6408e951a98717c97d7a3a80c6326b360
Jonathan <jonathan.silva@widigital.com.br>
2019-09-20 17:14:13 -0300

    Ajustes webcam

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # rubik-platform
+
+Este é um projeto de um kit educacional utilizando um cubo mágico (Rubik).
+
+### Pré-requisitos
+
+Para utilização do kit é necessario
+
+```
+Python
+O Kit educacional (robô)
+```
+
+### Instalação
+
+    Recomendavel a utilização de um virtual virtual environment
+    ```
+        * python3 -m venv 'caminho e nome do venv'
+    ```
+
+    Para as dependencias
+    ```
+    * pip install -r requirements.txt
+    ```
+
+    Para início
+    ```
+    * Executar o arquivo create_db.py para criar o banco novo
+    ```
+
+### Utilização
+
+    O usuario padrão é o admin:admin
+    As demais utilizações são explicadas na tela inicial
--- recoginition.py
+++ recoginition.py
@@ -15,12 +15,12 @@ FACES = {
 }
 
 CUBO = {
-    'Upper': {},
-    'Left': {},
-    'Front': {},
-    'Right': {},
-    'Back': {},
-    'Down': {},
+    FACES[1]: {},
+    FACES[2]: {},
+    FACES[3]: {},
+    FACES[4]: {},
+    FACES[5]: {},
+    FACES[6]: {},
 }
 
 
@@ -38,90 +38,194 @@ def get_cube():
     num_face = 1
 
     while True:
-        ret, frame = cap.read()
-        height, width = frame.shape[0], frame.shape[1]
-        point_x, point_y = ((width - draw_size)/2) + \
-            space_size, ((height - draw_size)/2) + space_size
-        rect = []
+        try:
+            ret, frame = cap.read()
+            height, width = frame.shape[0], frame.shape[1]
 
-        for i in range(0, 9):
-            if i % 3 != 0:
-                point_x += draw_step
-            else:
-                point_x = (width - draw_size)/2 + space_size
-                if i > 0:
-                    point_y += draw_step
-            point_x = int(point_x)
-            point_y = int(point_y)
-            rect.append([point_x, point_y])
-            cv.rectangle(
-                         frame,
-                         (point_x, point_y),
-                         (point_x+square_size, point_y + square_size),
-                         (255, 255, 255),
-                         2
-                        )
-        font = cv.FONT_HERSHEY_SIMPLEX
-        org = (50, 50)
-        fontScale = 1
-        thickness = 2
+            draw_square_size = 40
+            draw_init_x = width + 50
+            draw_init_y = 50
+            draw_height = draw_init_y*2 + draw_square_size*9
+            draw_init_y = (height > draw_height and int(abs(height - draw_square_size*9)/2) or draw_init_y)
+            draw_height = (height > draw_height and height or draw_height)
+            draw_width = draw_init_y*2 + draw_square_size*12
+            draw_width = int(width + draw_width)
+            frame_init = int(abs(height - draw_height)/2)
+            fundo = np.zeros(shape=[draw_height, draw_width, 3], dtype=np.uint8)
+            fundo[frame_init:height+frame_init, :width] = frame
+            frame = fundo
 
-        cv.putText(frame, 'Verificando: ' + FACES[num_face], org, font, fontScale, (255, 0, 0), thickness, cv.LINE_AA)
+            draw_cube = {
+                FACES[1]: {
+                    0: {'x': int(draw_init_x + draw_square_size*3), 'y': int(draw_init_y)},
+                    1: {'x': int(draw_init_x + draw_square_size*4), 'y': int(draw_init_y)},
+                    2: {'x': int(draw_init_x + draw_square_size*5), 'y': int(draw_init_y)},
+                    3: {'x': int(draw_init_x + draw_square_size*3), 'y': int(draw_init_y + draw_square_size)},
+                    4: {'x': int(draw_init_x + draw_square_size*4), 'y': int(draw_init_y + draw_square_size)},
+                    5: {'x': int(draw_init_x + draw_square_size*5), 'y': int(draw_init_y + draw_square_size)},
+                    6: {'x': int(draw_init_x + draw_square_size*3), 'y': int(draw_init_y + draw_square_size*2)},
+                    7: {'x': int(draw_init_x + draw_square_size*4), 'y': int(draw_init_y + draw_square_size*2)},
+                    8: {'x': int(draw_init_x + draw_square_size*5), 'y': int(draw_init_y + draw_square_size*2)},
+                },
+                FACES[2]: {
+                    0: {'x': int(draw_init_x), 'y': int(draw_init_y + draw_square_size*3)},
+                    1: {'x': int(draw_init_x + draw_square_size), 'y': int(draw_init_y + draw_square_size*3)},
+                    2: {'x': int(draw_init_x + draw_square_size*2), 'y': int(draw_init_y + draw_square_size*3)},
+                    3: {'x': int(draw_init_x), 'y': int(draw_init_y + draw_square_size*4)},
+                    4: {'x': int(draw_init_x + draw_square_size), 'y': int(draw_init_y + draw_square_size*4)},
+                    5: {'x': int(draw_init_x + draw_square_size*2), 'y': int(draw_init_y + draw_square_size*4)},
+                    6: {'x': int(draw_init_x), 'y': int(draw_init_y + draw_square_size*5)},
+                    7: {'x': int(draw_init_x + draw_square_size), 'y': int(draw_init_y + draw_square_size*5)},
+                    8: {'x': int(draw_init_x + draw_square_size*2), 'y': int(draw_init_y + draw_square_size*5)},
+                },
+                FACES[3]: {
+                    0: {'x': int(draw_init_x + draw_square_size*3), 'y': int(draw_init_y + draw_square_size*3)},
+                    1: {'x': int(draw_init_x + draw_square_size*4), 'y': int(draw_init_y + draw_square_size*3)},
+                    2: {'x': int(draw_init_x + draw_square_size*5), 'y': int(draw_init_y + draw_square_size*3)},
+                    3: {'x': int(draw_init_x + draw_square_size*3), 'y': int(draw_init_y + draw_square_size*4)},
+                    4: {'x': int(draw_init_x + draw_square_size*4), 'y': int(draw_init_y + draw_square_size*4)},
+                    5: {'x': int(draw_init_x + draw_square_size*5), 'y': int(draw_init_y + draw_square_size*4)},
+                    6: {'x': int(draw_init_x + draw_square_size*3), 'y': int(draw_init_y + draw_square_size*5)},
+                    7: {'x': int(draw_init_x + draw_square_size*4), 'y': int(draw_init_y + draw_square_size*5)},
+                    8: {'x': int(draw_init_x + draw_square_size*5), 'y': int(draw_init_y + draw_square_size*5)},
+                },
+                FACES[4]: {
+                    0: {'x': int(draw_init_x + draw_square_size*6), 'y': int(draw_init_y + draw_square_size*3)},
+                    1: {'x': int(draw_init_x + draw_square_size*7), 'y': int(draw_init_y + draw_square_size*3)},
+                    2: {'x': int(draw_init_x + draw_square_size*8), 'y': int(draw_init_y + draw_square_size*3)},
+                    3: {'x': int(draw_init_x + draw_square_size*6), 'y': int(draw_init_y + draw_square_size*4)},
+                    4: {'x': int(draw_init_x + draw_square_size*7), 'y': int(draw_init_y + draw_square_size*4)},
+                    5: {'x': int(draw_init_x + draw_square_size*8), 'y': int(draw_init_y + draw_square_size*4)},
+                    6: {'x': int(draw_init_x + draw_square_size*6), 'y': int(draw_init_y + draw_square_size*5)},
+                    7: {'x': int(draw_init_x + draw_square_size*7), 'y': int(draw_init_y + draw_square_size*5)},
+                    8: {'x': int(draw_init_x + draw_square_size*8), 'y': int(draw_init_y + draw_square_size*5)},
+                },
+                FACES[5]: {
+                    0: {'x': int(draw_init_x + draw_square_size*9), 'y': int(draw_init_y + draw_square_size*3)},
+                    1: {'x': int(draw_init_x + draw_square_size*10), 'y': int(draw_init_y + draw_square_size*3)},
+                    2: {'x': int(draw_init_x + draw_square_size*11), 'y': int(draw_init_y + draw_square_size*3)},
+                    3: {'x': int(draw_init_x + draw_square_size*9), 'y': int(draw_init_y + draw_square_size*4)},
+                    4: {'x': int(draw_init_x + draw_square_size*10), 'y': int(draw_init_y + draw_square_size*4)},
+                    5: {'x': int(draw_init_x + draw_square_size*11), 'y': int(draw_init_y + draw_square_size*4)},
+                    6: {'x': int(draw_init_x + draw_square_size*9), 'y': int(draw_init_y + draw_square_size*5)},
+                    7: {'x': int(draw_init_x + draw_square_size*10), 'y': int(draw_init_y + draw_square_size*5)},
+                    8: {'x': int(draw_init_x + draw_square_size*11), 'y': int(draw_init_y + draw_square_size*5)},
+                },
+                FACES[6]: {
+                    0: {'x': int(draw_init_x + draw_square_size*3), 'y': int(draw_init_y + draw_square_size*6)},
+                    1: {'x': int(draw_init_x + draw_square_size*4), 'y': int(draw_init_y + draw_square_size*6)},
+                    2: {'x': int(draw_init_x + draw_square_size*5), 'y': int(draw_init_y + draw_square_size*6)},
+                    3: {'x': int(draw_init_x + draw_square_size*3), 'y': int(draw_init_y + draw_square_size*7)},
+                    4: {'x': int(draw_init_x + draw_square_size*4), 'y': int(draw_init_y + draw_square_size*7)},
+                    5: {'x': int(draw_init_x + draw_square_size*5), 'y': int(draw_init_y + draw_square_size*7)},
+                    6: {'x': int(draw_init_x + draw_square_size*3), 'y': int(draw_init_y + draw_square_size*8)},
+                    7: {'x': int(draw_init_x + draw_square_size*4), 'y': int(draw_init_y + draw_square_size*8)},
+                    8: {'x': int(draw_init_x + draw_square_size*5), 'y': int(draw_init_y + draw_square_size*8)},
+                },
+            }
 
-        if(t.time() > time + 10 and num_face <= num_faces):
-            img = frame
-            time = t.time()
-            count = 0
-            face = FACES[num_face]
-            CUBO[face] = {}
-            for point in rect:
-                point_x, point_y = point[0], point[1]
-                area = img[point_y:point_y+square_size,
-                           point_x:point_x+square_size]
-                mean = contar_kmeans(area)
+            for face in CUBO:
+                for cube in draw_cube[face]:
+                    cv.rectangle(
+                                frame,
+                                (draw_cube[face][cube]['x'], draw_cube[face][cube]['y']),
+                                (draw_cube[face][cube]['x']+draw_square_size, draw_cube[face][cube]['y']+draw_square_size),
+                                (255, 255, 255),
+                                2
+                                )
+                    try:
+                        if CUBO[face][cube]:
+                            cv.rectangle(
+                                    frame,
+                                    (draw_cube[face][cube]['x'], draw_cube[face][cube]['y']),
+                                    (draw_cube[face][cube]['x']+draw_square_size, draw_cube[face][cube]['y']+draw_square_size),
+                                    get_rgb_by_color(CUBO[face][cube]),
+                                    cv.FILLED
+                                    )
+                    except:
+                        None
+            point_x, point_y = ((width - draw_size)/2) + \
+                space_size, ((height - draw_size)/2) + space_size
+            rect = []
+
+            for i in range(0, 9):
+                if i % 3 != 0:
+                    point_x += draw_step
+                else:
+                    point_x = (width - draw_size)/2 + space_size
+                    if i > 0:
+                        point_y += draw_step
+                point_x = int(point_x)
+                point_y = int(point_y)
+                rect.append([point_x, point_y])
                 cv.rectangle(
-                             img,
-                             (point_x, point_y),
-                             (point_x+square_size, point_y+square_size),
-                             (int(mean[0]), int(mean[1]), int(mean[2])),
-                             cv.FILLED
+                            frame,
+                            (point_x, point_y),
+                            (point_x+square_size, point_y + square_size),
+                            (255, 255, 255),
+                            2
                             )
-                color = get_colour_name(
-                    int(mean[0]), int(mean[1]), int(mean[2]))
-                CUBO[face][count] = color
-                count += 1
-            num_face += 1
+            font = cv.FONT_HERSHEY_SIMPLEX
+            org = (700, 40)
+            fontScale = 1
+            thickness = 2
 
-        if num_face > num_faces:
-            if verifica_cubo():
-                # texto
-                try:
-                    nome_arquivo = '../rubik-platform/uploads/input/in_texto.txt'
-                    arquivo = open(nome_arquivo, 'r+')
-                except FileNotFoundError:
-                    arquivo = open(nome_arquivo, 'w+')
-                for face in CUBO:
-                    for color in CUBO[face]:
-                        arquivo.writelines(CUBO[face][color][0])
-                arquivo.close()
+            cv.putText(frame, 'Verificando: ' + FACES[num_face], org, font, fontScale, (255, 0, 0), thickness, cv.LINE_AA)
 
-                # json
-                try:
-                    nome_arquivo = '../rubik-platform/uploads/input/in_json.txt'
-                    arquivo = open(nome_arquivo, 'r+')
-                except FileNotFoundError:
-                    arquivo = open(nome_arquivo, 'w+')
-                arquivo.writelines(json.dumps(CUBO))
-                arquivo.close()
+            if(t.time() > time + 10 and num_face <= num_faces):
+                img = frame
+                time = t.time()
+                count = 0
+                face = FACES[num_face]
+                CUBO[face] = {}
+                for point in rect:
+                    point_x, point_y = point[0], point[1]
+                    area = img[point_y:point_y+square_size,
+                            point_x:point_x+square_size]
+                    mean = contar_kmeans(area)
+                    cv.rectangle(
+                                img,
+                                (point_x, point_y),
+                                (point_x+square_size, point_y+square_size),
+                                (int(mean[0]), int(mean[1]), int(mean[2])),
+                                cv.FILLED
+                                )
+                    color = get_colour_name(
+                        int(mean[0]), int(mean[1]), int(mean[2]))
+                    CUBO[face][count] = color
+                    count += 1
+                num_face += 1
 
+            if num_face > num_faces:
+                if verifica_cubo():
+                    # texto
+                    try:
+                        nome_arquivo = '../rubik-platform/uploads/input/in_texto.txt'
+                        arquivo = open(nome_arquivo, 'r+')
+                    except FileNotFoundError:
+                        arquivo = open(nome_arquivo, 'w+')
+                    for face in CUBO:
+                        for color in CUBO[face]:
+                            arquivo.writelines(CUBO[face][color][0])
+                    arquivo.close()
+
+                    # json
+                    try:
+                        nome_arquivo = '../rubik-platform/uploads/input/in_json.txt'
+                        arquivo = open(nome_arquivo, 'r+')
+                    except FileNotFoundError:
+                        arquivo = open(nome_arquivo, 'w+')
+                    arquivo.writelines(json.dumps(CUBO))
+                    arquivo.close()
+
+                    break
+                else:
+                    num_face = 1
+
+            cv.imshow('rubik_capture', frame)
+            if cv.waitKey(1) & 0xFF == ord('q'):
                 break
-            else:
-                num_face = 1
-
-        cv.imshow('rubik_capture', frame)
-        if img.any():
-            cv.imshow('img', img)
-        if cv.waitKey(1) & 0xFF == ord('q'):
+        except:
             break
 
     cap.release()
@@ -208,6 +312,21 @@ def get_colour_name(b, g, r):
         color = "w"
 
     return color
+
+
+def get_rgb_by_color(color):
+    if color == 'r':
+        return (0, 0, 255)
+    elif color == 'o':
+        return (0, 125, 255)
+    elif color == 'y':
+        return (0, 255, 255)
+    elif color == 'g':
+        return (0, 255, 0)
+    elif color == 'b':
+        return (255, 0, 0)
+    elif color == 'w':
+        return (255, 255, 255)
 
 
 if __name__ == '__main__':
